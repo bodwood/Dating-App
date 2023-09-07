934787ce29bfd991dbdfaa543680239c817bba93
bodwood <bodiewood@hotmail.com>
2023-09-07 11:13:27 -0700

    Add HTTP routing

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,40 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")] // https://localhost:5001/api/users
-    public class UsersController : ControllerBase
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
     {
-        // Field that can be used with all methods.
-        // Allows us to access our database.
-        private readonly DataContext _context;
+        _context = context;
+    }
 
-        public UsersController(DataContext context)
-        {
-            _context = context;
-        }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync(); 
+        return users;
+    }
 
-        // Returns a list of users.
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-        {
-            // Returns a list of users.
-            return await _context.Users.ToListAsync();
-        }
-
-        [HttpGet("{id}")] // https://localhost:5001/api/users/{id}
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-        {
-            // Returns a user with the specified id.
-            return await _context.Users.FindAsync(id);
-        }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,22 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Add the DataContext to the services container.
-builder.Services.AddDbContext<DataContext>(opt =>
+builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
